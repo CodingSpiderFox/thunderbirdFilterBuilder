0a6e127580d7f5d53128b697b6eb516a62918a72
CodingSpiderFox <codingspiderfox@gmail.com>
2019-08-08 18:26:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,34 @@ import java.io.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main( String[] args ) throws IOException
+    {
         File resultFile = new File("result");
-        String result= "";
+        String result = "";
 
         /**
-         * version="9"
-         logging="no"
-         name="Subject contains: text"
-         enabled="yes"
-         type="17"
-         action="Move to folder"
-         actionValue="mailbox://nobody@Feeds/Trash"
-         condition="OR (subject,contains,text) OR (body,contains,text)"
+         * version="9" logging="no" name="Subject contains: text" enabled="yes" type="17" action="Move to
+         * folder" actionValue="mailbox://nobody@Feeds/Trash" condition="OR (subject,contains,text) OR
+         * (body,contains,text)"
          */
 
-        result = result + ("version=9\n")
-                +("logging=no\r\n")
-                +("name=\"global\"\n")
-                +("enabled=\"yes\"\n")
-                +("type=\"17\"\n")
-                +("action=\"Move to folder\"\n")
-                +("actionValue=\"mailbox://nobody@Feeds/Thrash\"\n")
-                +("condition=\"\n");
+        result = result + ("version=\"9\"\n") + ("logging=\"yes\"\r\n") + ("name=\"global\"\n")
+            + ("enabled=\"yes\"\n")
+            + ("type=\"17\"\n") + ("action=\"Move to folder\"\n")
+            + ("actionValue=\"mailbox://nobody@Feeds/Thrash\"\n") + ("condition=\"");
 
-        File input = new File("input_shared");
+        File input = new File( "input_shared" );
 
         try (BufferedReader br = new BufferedReader(new FileReader(input))) {
             String line;
-            while((line = br.readLine()) != null) {
-                result = result+("OR (subject,contains,)") + line + ") OR (body,contains," + line + ")\"\n";
+            while( (line = br.readLine()) != null )
+            {
+                result = result + ("OR (subject,contains,") + line + ") OR (body,contains," + line + ") ";
             }
+            result += "\"";
         }
 
-        System.out.println(result);
+        System.out.println( result );
     }
     
 }
