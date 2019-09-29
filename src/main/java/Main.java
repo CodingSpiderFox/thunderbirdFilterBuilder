057da7e24f8b4685e8b54ae365bfba576954d56b
CodingSpiderFox <codingspiderfox@gmail.com>
2019-09-29 10:51:49 +0200

    add logic to generate seperate filter rules for each keyword

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,42 @@ public class Main {
 
     public static void main( String[] args ) throws IOException
     {
+        File input = new File( "input_shared" );
+
+        String result = writeAllKeywordsToSingleFilterRule(input);
+        System.out.println( result );
+        result = writeAllKeywordsToSeparateFilterRules(input);
+        System.out.println( result );
+    }
+
+    private static String writeAllKeywordsToSeparateFilterRules(File input) throws IOException {
+        File resultFile = new File("result");
+        String result = "";
+
+        /**
+         * version="9" logging="no" name="Subject contains: text" enabled="yes" type="17" action="Move to
+         * folder" actionValue="mailbox://nobody@Feeds/Trash" condition="OR (subject,contains,text) OR
+         * (body,contains,text)"
+         */
+
+        result = result + ("version=\"9\"\n") + ("logging=\"yes\"\r\n") ;
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+            String line;
+            while( (line = br.readLine()) != null )
+            {
+                result += ("name=\"subject contains " + line + "\"\r\n")
+                        + ("enabled=\"yes\"\n")
+                        + ("type=\"17\"\n") + ("action=\"Move to folder\"\n")
+                        + ("actionValue=\"mailbox://nobody@Feeds/Thrash\"\n") + ("condition=\"");
+                result += ("AND (subject,contains,") + line + ")\"\r\n";
+            }
+        }
+        return result;
+    }
+
+    private static String writeAllKeywordsToSingleFilterRule(File input) throws IOException {
         File resultFile = new File("result");
         String result = "";
 
@@ -14,22 +50,19 @@ public class Main {
          */
 
         result = result + ("version=\"9\"\n") + ("logging=\"yes\"\r\n") + ("name=\"global\"\n")
-            + ("enabled=\"yes\"\n")
-            + ("type=\"17\"\n") + ("action=\"Move to folder\"\n")
-            + ("actionValue=\"mailbox://nobody@Feeds/Thrash\"\n") + ("condition=\"");
+                + ("enabled=\"yes\"\n")
+                + ("type=\"17\"\n") + ("action=\"Move to folder\"\n")
+                + ("actionValue=\"mailbox://nobody@Feeds/Thrash\"\n") + ("condition=\"");
 
-        File input = new File( "input_shared" );
 
         try (BufferedReader br = new BufferedReader(new FileReader(input))) {
             String line;
-            while( (line = br.readLine()) != null )
-            {
+            while ((line = br.readLine()) != null) {
                 result = result + ("OR (subject,contains,") + line + ") OR (body,contains," + line + ") ";
             }
             result += "\"";
         }
-
-        System.out.println( result );
+        return result;
     }
-    
+
 }
